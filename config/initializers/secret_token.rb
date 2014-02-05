e3d82596f558d171694f1cc20a661f2356d39862
el3ctro- <josh.el3@gmail.com>
2014-02-04 18:32:51 -0800

    Intial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Osrpg::Application.config.secret_key_base = '36c16bb2dad9c5356d1b0023e743e94ccc43660351ef1454d4f76f9035067f09c25380fe049970ac92bb44f49c3a7cf378af907e583b0f03e79b52cdca794adc'
