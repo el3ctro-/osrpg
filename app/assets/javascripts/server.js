58265eb9c0c9e7e39e15457352c30cba7bca2919
el3ctro- <josh.el3@gmail.com>
2014-02-19 13:16:43 -0800

    Update server.js

--- app/assets/javascripts/server.js
+++ app/assets/javascripts/server.js
@@ -1,43 +1,53 @@
 var http = require('http');
 
 // Roll to see if monster moves north, south, east, or west
+var monsters = [];
 var rollDice;
 var monsterPosition;
 var newmonsterPosition;
 monsterPosition = 0;
 var another;
+var whichmonster = 0;
 setInterval(function(){
-	rollDice = Math.floor((Math.random()*4)+1);
-  	switch (rollDice) {
-	case 1:
-	// North
-	monsterPosition -= 20;
-	break;
-	case 2:
-	// East
-	monsterPosition += 1;
-	break;
-	case 3:
-	// South
-	monsterPosition += 20;
-	break;
-	case 4:
-	// West
-	monsterPosition -= 1;
-	newmonsterPosition = { position: 4 };
-	break;
+rollDice = Math.floor((Math.random()*4)+1);
+   switch (rollDice) {
+case 1:
+// North
+monsterPosition -= 20;
+monsters[whichmonster] = (monsterPosition);
+break;
+case 2:
+// East
+monsterPosition += 1;
+monsters[whichmonster] = (monsterPosition);
+break;
+case 3:
+// South
+monsterPosition += 20;
+monsters[whichmonster] = (monsterPosition);
+break;
+case 4:
+// West
+monsterPosition -= 1;
+monsters[whichmonster] = (monsterPosition);
+var newmonsterPosition = { position: 4, f: function() {
+    return this.position;
+	}
+	};
+break;
 };
 
 },6);
 
 http.createServer(function (req, res) {
+
 var amonsterPosition = 15;
-	var newmonsterPosition = { name: "monster0", position: monsterPosition, shit: "fuck" };
+var newmonsterPosition = { name: "monster0", position: monsters , a: "bc" };
   res.writeHead(200, {"Content-Type": "application/json"});
-//  var otherArray = ["item1", "item2"];
+// var otherArray = ["item1", "item2"];
   var otherObject = { item1: "item1val", item2: "item2val" };
 
-  var json = JSON.stringify({ 
+  var json = JSON.stringify({
 
    another: Math.floor((Math.random()*1000)+1),
    payload: newmonsterPosition,
